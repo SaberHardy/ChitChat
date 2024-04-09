52bcdbf5e0ef4574adacfe83a828393851acff25
SaberHardy <samirsamado5@gmail.com>
2024-04-08 23:29:52 -0700

    login/register

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def hello_world():
     return render_template('chit_chat/index.html')
 
 
+@app.route('/login-user', methods=['GET', 'POST'])
+def login():
+    return render_template('chit_chat/login.html')
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def register():
+    return render_template('chit_chat/register.html')
+
+
 if __name__ == '__main__':
     app.run()
